04ef4251a121bbd0b5fe94d34358b9521bdf7b0b
Vanshu Galhotra <vanshugalhotra3332@gmail.com>
2026-02-22 12:07:57 +0530

    backend: [auth] added Auth Utils

--- backend/src/config/config.service.ts
+++ backend/src/config/config.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import * as dotenv from 'dotenv';
 import * as fs from 'fs';
 import * as Joi from 'joi';
+import { StringValue } from 'ms';
 
 export interface AppConfig {
   DATABASE_URL: string;
@@ -11,8 +12,16 @@ export interface AppConfig {
   FRONTEND_URL: string;
   ALLOWED_ORIGINS: string;
 
-  JWT_SECRET: string;
-  JWT_EXPIRES_IN: string;
+  JWT_ACCESS_SECRET: string;
+  JWT_REFRESH_SECRET: string;
+  JWT_ACCESS_EXPIRES_IN: StringValue;
+  JWT_REFRESH_EXPIRES_IN: StringValue;
+
+  AUTH_COOKIE_NAME: string;
+  AUTH_COOKIE_SECURE: boolean;
+  AUTH_COOKIE_SAMESITE: string;
+  AUTH_COOKIE_PATH: string;
+  AUTH_ADMIN_SECRET: string;
 
   LOG_LEVEL: 'fatal' | 'error' | 'warn' | 'info' | 'debug' | 'trace';
 }
@@ -41,8 +50,16 @@ export class ConfigService {
       FRONTEND_URL: Joi.string().required(),
       ALLOWED_ORIGINS: Joi.string().required(),
 
-      JWT_SECRET: Joi.string().required(),
-      JWT_EXPIRES_IN: Joi.string().required(),
+      JWT_ACCESS_SECRET: Joi.string().required(),
+      JWT_REFRESH_SECRET: Joi.string().required(),
+      JWT_ACCESS_EXPIRES_IN: Joi.string().required(),
+      JWT_REFRESH_EXPIRES_IN: Joi.string().required(),
+
+      AUTH_COOKIE_NAME: Joi.string().required(),
+      AUTH_COOKIE_SECURE: Joi.boolean().default(false),
+      AUTH_COOKIE_SAMESITE: Joi.string().required(),
+      AUTH_COOKIE_PATH: Joi.string().required(),
+      AUTH_ADMIN_SECRET: Joi.string().required(),
 
       LOG_LEVEL: Joi.string()
         .valid('fatal', 'error', 'warn', 'info', 'debug', 'trace')

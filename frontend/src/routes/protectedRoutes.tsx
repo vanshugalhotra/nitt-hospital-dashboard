bea5f0d3437fd81e966312a662dbe78e47e80103
Chirag Tomar <chiragtomar251@gmail.com>
2026-02-26 11:04:49 +0530

    /me endpoint working and profile feature working fine.

--- frontend/src/routes/protectedRoutes.tsx
+++ frontend/src/routes/protectedRoutes.tsx
@@ -1,21 +1,29 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "@/hooks/use-auth";
+import { ReactNode } from "react";
+import { AuthUser } from "@/types/auth";
 
-const ProtectedRoute = ({ children, allowedRole }) => {
+interface ProtectedRouteProps {
+  children: ReactNode;
+  allowedRole?: AuthUser["role"];
+}
+
+const ProtectedRoute = ({ children, allowedRole }: ProtectedRouteProps) => {
   const { data: user, isLoading, isError } = useAuth();
-   if (isLoading) {
+
+  if (isLoading) {
     return <div>Checking authentication...</div>;
   }
 
-   if (isError || !user) {
+  if (isError || !user) {
     return <Navigate to="/login" replace />;
   }
 
-    if (allowedRole && user.role !== allowedRole) {
+  if (allowedRole && user.role !== allowedRole) {
     return <Navigate to={`/${user.role}`} replace />;
   }
 
-    return children;
+  return <>{children}</>;
 };
 
 export default ProtectedRoute;
